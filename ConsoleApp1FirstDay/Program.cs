4506e047eed2f09d37b2e05f879e34e517f0329f
Vivian Dak <raghadvd.777@gmail.com>
2019-05-27 14:31:35 +0200

    init

---
+++ ConsoleApp1FirstDay/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1FirstDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num > 5)
+            {
+                Console.WriteLine("value is bigger than 5");
+            }
+            else
+            {
+                Console.WriteLine("value is not bigger than 5");
+            }
+            
+        }
+    }
+}
